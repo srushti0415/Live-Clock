8c4aaf0ae5246e18e5d8c8f52e9c7cd31de0e86f
srushti0415 <srushtikulkarni15@gmail.com>
2021-09-21 21:14:52 +0530

    Clock

---
+++ index2.js
@@ -0,0 +1,13 @@
+function SClock() {
+    var date = new Date;
+
+    var hours = date.getHours();
+    var minutes = date.getMinutes();
+    var seconds = date.getSeconds();
+
+    var ampm = (hours < 12) ? "AM" : "PM";
+    document.getElementById('TIME').innerHTML =
+        hours + " : " + minutes + " : " + seconds + " " + ampm;
+    setTimeout(SClock, 500);
+
+}
